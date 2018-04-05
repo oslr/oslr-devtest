5b909afabdd68ba46055dc530a4f401b1d211fea
Adam Pennycuick <adampennycuick@gmail.com>
2018-04-05 21:43:10 +0100

    Update Mirage config

--- mirage/config.js
+++ mirage/config.js
@@ -24,25 +24,12 @@ export default function() {
     http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
   */
 
-  this.get('/users', () => {
-    return {
-      users: [
-        {id: 1, name: 'Adam Pennycuick'},
-        {id: 2, name: 'Alan Pooley'},
-        {id: 3, name: 'Tom Simpson'},
-        {id: 4, name: 'Corinne Kennard'},
-        {id: 5, name: 'Maude Disney'},
-        {id: 6, name: 'Ilbert Key'},
-        {id: 7, name: 'Christopher Fields'},
-        {id: 8, name: 'Glen Bates'},
-        {id: 9, name: 'Lizbeth Jakeman'},
-        {id: 10, name: 'Jessi Causey'},
-        {id: 11, name: 'Brande Daniell'},
-        {id: 12, name: 'Darcy Mason'},
-        {id: 13, name: 'Maxene Walmsley'},
-        {id: 14, name: 'Azura Kinsley'},
-        {id: 15, name: 'Kaye Elliott'}
-      ]
-    };
+  /*
+  This defines our API for users. It returns all users with no parameters.
+  If params are given, it returns users matching exactly those params (e.g. if name = "User 9" it will return only User 9)
+  */
+  this.get('/users', (schema, request) => {
+    return schema.users.where(request.queryParams);
   });
+
 }
---
+++ mirage/factories/user.js
@@ -0,0 +1,13 @@
+import { Factory } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  name(i) {
+    var name;
+    if(i < 10){
+      name = `Student ${i+1}`;
+    }else{
+      name = `Doctor ${i+1}`;
+    }
+    return name;
+  }
+});
